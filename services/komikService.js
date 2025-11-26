f3512847198cc8f8bfde3544f142039388bbf22f
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-26 12:32:02 +0700

    memperbaiki

--- services/komikService.js
+++ services/komikService.js
@@ -1,68 +1,61 @@
 async function createKomik(database, komikData) {
-  const { judul, deskripsi, penulis, imageType, imageName, imageData } = komikData;
+    // Gunakan destructuring field Bahasa Indonesia sesuai Postman & Model
+    const { judul, deskripsi, penulis, imageType, imageName, imageData } = komikData;
 
-  if (!judul || !deskripsi || !penulis) {
-    throw new Error('Title, description, dan author wajib diisi');
-  }
+    // Validasi input
+    if (!judul || !deskripsi || !penulis) {
+        throw new Error('Judul, deskripsi, dan penulis wajib diisi');
+    }
 
-  const newKomik = await database.Komik.create({
-    judul,
-    deskripsi,
-    penulis,
-    imageType: imageType || null,
-    imageName: imageName || null,
-    imageData: imageData || null,
-  });
+    const newKomik = await database.Komik.create({
+        judul,
+        deskripsi,
+        penulis,
+        imageType: imageType || null,
+        imageName: imageName || null,
+        ImageData: imageData || null,
+    });
 
-  return newKomik;
+    return newKomik;
 }
 
 async function getAllKomik(database) {
-  const komiks = await database.Komik.findAll();
-
-  return komiks.map(k => {
-    if (k.imageData) {
-        k.imageData = k.imageData.toString('base64');
-    }
-    return k;
-  });
+    const komiks = await database.Komik.findAll();
+    return komiks.map(k => {
+        if (k.imageData) {
+            k.imageData = k.imageData.toString('base64');
+        }
+        return k;
+    });
 }
 
+// ... (Fungsi getKomikById, updateKomik, deleteKomik lainnya tetap sama, pastikan import module.exports benar)
+
 async function getKomikById(database, id) {
-  const komik = await database.Komik.findByPk(id);
-  if (!komik) throw new Error('Komik tidak ditemukan');
-
-  if (komik.imageData) {
-      komik.imageData = komik.imageData.toString('base64');
-  }
-
-  return komik;
+    const komik = await database.Komik.findByPk(id);
+    if (!komik) throw new Error('Komik tidak ditemukan');
+    if (komik.imageData) komik.imageData = komik.imageData.toString('base64');
+    return komik;
 }
 
 async function updateKomik(database, id, komikData) {
-  const komik = await database.Komik.findByPk(id);
-  if (!komik) {
-    throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
-  }
-
-  await komik.update(komikData);
-  return komik;
+    const komik = await database.Komik.findByPk(id);
+    if (!komik) throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
+    await komik.update(komikData);
+    return komik;
 }
 
 async function deleteKomik(database, id) {
-  const komik = await database.Komik.findByPk(id);
-  if (!komik) {
-    throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
-  }
-
-  await komik.destroy();
-  return { message: `Komik dengan ID ${id} berhasil dihapus` };
+    const komik = await database.Komik.findByPk(id);
+    if (!komik) throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
+    await komik.destroy();
+    return { message: `Komik dengan ID ${id} berhasil dihapus` };
 }
 
 module.exports = {
-  createKomik,
-  getAllKomik,
-  getKomikById,
-  updateKomik,
-  deleteKomik,
+    createKomik,
+    getAllKomik,
+    getKomikById,
+    updateKomik,
+    deleteKomik,
 };
